ff54a387efb00358f1bf462082ed714bd77bfc13
Beatriz Borges Barros <88691372+beatrizborgesb@users.noreply.github.com>
2022-09-30 20:38:54 -0300

    adicionado botão para subir a página

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
-document.addEventListener("DOMContentLoaded", function (event) {
-  document.body.innerHTML = `
+document.body.innerHTML = `
   <div class="container">
   <h1>AUtitude - Leitor de textos com voz!</h1>
 
@@ -8,9 +7,10 @@ document.addEventListener("DOMContentLoaded", function (event) {
 
   <button class="btn btn-toggle">Inserir texto</button>
 
-  <label for="switch">
+  <label for="switch"> Dark Mode
     <input type="checkbox" id="switch" />
   </label>
+
 
   <div class="text-box">
     <div class="close">X</div>
@@ -31,144 +31,144 @@ document.addEventListener("DOMContentLoaded", function (event) {
 <nav></nav>
   `;
 
-  const main = document.querySelector("main");
-  const buttonInsertText = document.querySelector(".btn-toggle");
-  const buttonReadText = document.querySelector("#read");
-  const divTextBox = document.querySelector(".text-box");
-  const closeDivTextBox = document.querySelector(".close");
-  const selectElement = document.querySelector("select");
-  const textArea = document.querySelector("textArea");
+const main = document.querySelector("main");
+const buttonInsertText = document.querySelector(".btn-toggle");
+const buttonReadText = document.querySelector("#read");
+const divTextBox = document.querySelector(".text-box");
+const closeDivTextBox = document.querySelector(".close");
+const selectElement = document.querySelector("select");
+const textArea = document.querySelector("textArea");
 
-  const humanExpressions = [
-    { img: "./img/drink.png", text: "Estou com sede" },
-    { img: "./img/angry.png", text: "Estou bravo" },
-    { img: "./img/grandma.png", text: "Quero a vovó" },
-    { img: "./img/outside.png", text: "Quero ir lá fora" },
-    { img: "./img/sad.png", text: "Estou triste" },
-    { img: "./img/scared.png", text: "Estou assustado" },
-    { img: "./img/school.png", text: "Quero ir para a escola" },
-    { img: "./img/tired.png", text: "Estou cansado" },
-    { img: "./img/food.png", text: "Estou com fome" },
-    { img: "./img/happy.png", text: "Estou feliz" },
-    { img: "./img/home.png", text: "Quero ir para casa" },
-    { img: "./img/hurt.png", text: "Estou machucado" },
-    { img: "./img/sleep.png", text: "Estou com sono" },
-    { img: "./img/restroom.png", text: "Quero ir ao banheiro" },
-    { img: "./img/pipa.png", text: "Quero soltar pipa" },
-    { img: "./img/hugg.png", text: "Quero um abraço" },
-  ];
+const humanExpressions = [
+  { img: "./img/drink.png", text: "Estou com sede" },
+  { img: "./img/angry.png", text: "Estou bravo" },
+  { img: "./img/grandma.png", text: "Quero a vovó" },
+  { img: "./img/outside.png", text: "Quero ir lá fora" },
+  { img: "./img/sad.png", text: "Estou triste" },
+  { img: "./img/scared.png", text: "Estou assustado" },
+  { img: "./img/school.png", text: "Quero ir para a escola" },
+  { img: "./img/tired.png", text: "Estou cansado" },
+  { img: "./img/food.png", text: "Estou com fome" },
+  { img: "./img/happy.png", text: "Estou feliz" },
+  { img: "./img/home.png", text: "Quero ir para casa" },
+  { img: "./img/hurt.png", text: "Estou machucado" },
+  { img: "./img/sleep.png", text: "Estou com sono" },
+  { img: "./img/restroom.png", text: "Quero ir ao banheiro" },
+  { img: "./img/pipa.png", text: "Quero soltar pipa" },
+  { img: "./img/hugg.png", text: "Quero um abraço" },
+];
 
-  const utterance = new SpeechSynthesisUtterance();
+const utterance = new SpeechSynthesisUtterance();
 
-  const setTextMessage = (text) => {
-    utterance.text = text;
-  };
+const setTextMessage = (text) => {
+  utterance.text = text;
+};
 
-  const speakText = () => {
-    speechSynthesis.speak(utterance);
-  };
+const speakText = () => {
+  speechSynthesis.speak(utterance);
+};
 
-  const setVoice = (event) => {
-    const selectedVoice = voices.find(
-      (voice) => voice.name === event.target.value
-    );
-    utterance.voice = selectedVoice;
-  };
+const setVoice = (event) => {
+  const selectedVoice = voices.find(
+    (voice) => voice.name === event.target.value
+  );
+  utterance.voice = selectedVoice;
+};
 
-  const addExpressionBoxesIntoDOM = () => {
-    main.innerHTML = humanExpressions
-      .map(
-        ({ img, text }) => `
+const addExpressionBoxesIntoDOM = () => {
+  main.innerHTML = humanExpressions
+    .map(
+      ({ img, text }) => `
    <div class= "expression-box" data-js="${text}"> 
       <img src="${img}" alt="${text}" data-js="${text}">
       <p class="info" data-js="${text}">${text}</p>
    </div>
   `
-      )
-      .join("");
-  };
-  addExpressionBoxesIntoDOM();
+    )
+    .join("");
+};
+addExpressionBoxesIntoDOM();
 
-  const setStyleOfClickedDiv = (dataValue) => {
-    const div = document.querySelector(`[data-js="${dataValue}"]`);
-    div.classList.add("active");
-    setTimeout(() => {
-      div.classList.remove("active");
-    }, 1000);
-  };
+const setStyleOfClickedDiv = (dataValue) => {
+  const div = document.querySelector(`[data-js="${dataValue}"]`);
+  div.classList.add("active");
+  setTimeout(() => {
+    div.classList.remove("active");
+  }, 1000);
+};
 
-  main.addEventListener("click", (event) => {
-    const clickedElement = event.target;
-    const clickedElementText = clickedElement.dataset.js;
-    const clickedElementTextMustBeSpoken = ["img", "p"].some(
-      (elementName) =>
-        clickedElement.tagName.toLowerCase() === elementName.toLowerCase()
-    );
+main.addEventListener("click", (event) => {
+  const clickedElement = event.target;
+  const clickedElementText = clickedElement.dataset.js;
+  const clickedElementTextMustBeSpoken = ["img", "p"].some(
+    (elementName) =>
+      clickedElement.tagName.toLowerCase() === elementName.toLowerCase()
+  );
 
-    if (clickedElementTextMustBeSpoken) {
-      setTextMessage(clickedElementText);
-      speakText();
-      setStyleOfClickedDiv(clickedElementText);
-    }
-  });
-
-  const insertOptionElementsIntoDOM = (voices) => {
-    selectElement.innerHTML = voices.reduce((accumulator, { name, lang }) => {
-      accumulator += `<option value="${name}">${lang}' | ${name}</option>`;
-      return accumulator;
-    }, "");
-  };
-
-  const setUtteranceVoice = (voice) => {
-    utterance.voice = voice;
-    const voiceOptionElement = selectElement.querySelector(
-      `[value="${voice.name}"]"`
-    );
-    voiceOptionElement.selected = true;
-  };
-
-  const setPTBRVoices = (voices) => {
-    const googleVoice = voices.find(
-      (voice) => voice.name === "Google português do Brasil"
-    );
-    const microsoftVoice = voices.find(
-      (voice) => voice.name === "Microsoft Maria Desktop - Portuguese(Brazil)"
-    );
-
-    if (googleVoice) {
-      setUtteranceVoice(googleVoice);
-    } else if (microsoftVoice) {
-      ssetUtteranceVoice(microsoftVoice);
-    }
-  };
-
-  let voices = [];
-
-  speechSynthesis.addEventListener("voiceschanged", () => {
-    voices = speechSynthesis.getVoices();
-
-    insertOptionElementsIntoDOM(voices);
-    setPTBRVoices(voices);
-  });
-
-  buttonInsertText.addEventListener("click", () => {
-    divTextBox.classList.add("show");
-  });
-
-  closeDivTextBox.addEventListener("click", () => {
-    divTextBox.classList.remove("show");
-  });
-
-  selectElement.addEventListener("change", setVoice);
-
-  buttonReadText.addEventListener("click", () => {
-    setTextMessage(textArea.value);
+  if (clickedElementTextMustBeSpoken) {
+    setTextMessage(clickedElementText);
     speakText();
-  });
+    setStyleOfClickedDiv(clickedElementText);
+  }
+});
 
-  const nav = document.createElement("nav");
+const insertOptionElementsIntoDOM = (voices) => {
+  selectElement.innerHTML = voices.reduce((accumulator, { name, lang }) => {
+    accumulator += `<option value="${name}">${lang}' | ${name}</option>`;
+    return accumulator;
+  }, "");
+};
 
-  nav.innerHTML = `
+const setUtteranceVoice = (voice) => {
+  utterance.voice = voice;
+  const voiceOptionElement = selectElement.querySelector(
+    `[value="${voice.name}"]"`
+  );
+  voiceOptionElement.selected = true;
+};
+
+const setPTBRVoices = (voices) => {
+  const googleVoice = voices.find(
+    (voice) => voice.name === "Google português do Brasil"
+  );
+  const microsoftVoice = voices.find(
+    (voice) => voice.name === "Microsoft Maria Desktop - Portuguese(Brazil)"
+  );
+
+  if (googleVoice) {
+    setUtteranceVoice(googleVoice);
+  } else if (microsoftVoice) {
+    ssetUtteranceVoice(microsoftVoice);
+  }
+};
+
+let voices = [];
+
+speechSynthesis.addEventListener("voiceschanged", () => {
+  voices = speechSynthesis.getVoices();
+
+  insertOptionElementsIntoDOM(voices);
+  setPTBRVoices(voices);
+});
+
+buttonInsertText.addEventListener("click", () => {
+  divTextBox.classList.add("show");
+});
+
+closeDivTextBox.addEventListener("click", () => {
+  divTextBox.classList.remove("show");
+});
+
+selectElement.addEventListener("change", setVoice);
+
+buttonReadText.addEventListener("click", () => {
+  setTextMessage(textArea.value);
+  speakText();
+});
+
+const nav = document.createElement("nav");
+
+nav.innerHTML = `
       <div classname="links">    
           <a href="#Home">Home</a>
       
@@ -180,16 +180,31 @@ document.addEventListener("DOMContentLoaded", function (event) {
   
 `;
 
-  const container = document.querySelector(".container");
+const container = document.querySelector(".container");
 
-  container.before(nav);
+container.before(nav);
+
+const footer = document.createElement("footer");
+
+const upButton = document.createElement("button");
+
+upButton.id = "upButton";
+
+upButton.innerHTML = `
+  <img src="./scrolltop.png" /> 
+`;
+
+footer.appendChild(upButton);
+
+document.body.appendChild(footer);
+
+upButton.addEventListener("click", () => {
+  window.scrollTo(0, 0);
 });
-
-const nav = document.getElementsByTagName("nav");
 
 const $html = document.querySelector("html");
 const $checkbox = document.querySelector("#switch");
+
 $checkbox.addEventListener("change", function () {
   $html.classList.toggle("dark-mode");
-  nav[0].classList.toggle("dark-mode");
 });
